6e63adf355634ddd563ab686365ba7179bed498d
Ryan Cook <rcook@sincerususa.com>
2020-04-05 16:17:07 -0400

    - Moved Entry Point script to correct location - added missing files

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,17 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 api = Api(app)
 
 app.config.from_object('project.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 class UsersPing(Resource):
     def get(self):
         return {
-                "status":"success",
-                "message":"pong!"
-                }
+                "status" : "success",
+                "message": "pong!"
+        }
+
 
 api.add_resource(UsersPing, '/users/ping')
