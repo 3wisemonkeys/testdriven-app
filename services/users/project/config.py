c96d25f3ac39c7617a5ef26437ddf07abd28df22
Ryan Cook <rcook@sincerususa.com>
2020-04-06 21:47:37 -0400

    travis ci

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,22 +1,27 @@
-import os
+# services/users/project/config.py
+
+
+import os  # new
 
 
 class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'  # new
+    SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
