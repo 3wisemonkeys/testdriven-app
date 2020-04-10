eabf2af11ed631e373cdc80141f6e8085b2991a0
Ryan Cook <rcook@sincerususa.com>
2020-04-09 22:47:27 -0400

    React Users AddUser finished

--- services/client/src/components/__test__/UsersList.test.jsx
+++ services/client/src/components/__test__/UsersList.test.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import UsersList from '../UsersList';
+import renderer from 'react-test-renderer';
 
 const users = [
   {
     'active': true,
-    'email': 'hermanmu@gmail.com',
+    'email': 'ryan@3wisemonkeys.org',
     'id': 1,
-    'username': 'michael'
+    'username': 'ryan'
   },
   {
     'active': true,
-    'email': 'michael@mherman.org',
+    'email': 'nichelle@3wisemonkeys.org',
     'id': 2,
-    'username': 'michaelherman'
+    'username': 'ryan'
   }
 ];
 
@@ -22,5 +23,10 @@ test('UsersList renders properly', () => {
   const wrapper = shallow(<UsersList users={users}/>);
   const element = wrapper.find('h4');
   expect(element.length).toBe(2);
-  expect(element.get(0).props.children).toBe('michael');
+  expect(element.get(0).props.children).toBe('ryan');
+});
+
+test('UsersList renders a snapshot properly', () => {
+  const tree = renderer.create(<UsersList users={users}/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,20 +2,38 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from "./components/AddUser";
 
 class App extends Component {
-    constructor() {
+   constructor() {
         super();
-        // new
-        this.state = {
-            users: []
-        };
-    };
+          this.state = {
+              users: [],
+              username: '', // new
+              email: '',    // new
+          };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+   };
 
     componentDidMount() {
         this.getUsers();
     }
-
+    addUser(event) {
+      event.preventDefault();
+      // new
+      const data = {
+        username: this.state.username,
+        email: this.state.email
+      };
+      // new
+      axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+          this.getUsers();  // new
+          this.setState({ username: '', email: '' });
+      })
+      .catch((err) => { console.log(err); });
+    };
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)  // new
             .then((res) => {
@@ -25,27 +43,33 @@ class App extends Component {
                 console.log(err);
             });
     };
-
+    handleChange(event) {
+      const obj = {};
+      obj[event.target.name] = event.target.value;
+      this.setState(obj);
+    };
     render() {
         return (
             <section className="section">
                 <div className="container">
                     <div className="columns">
-                        <div className="column is-one-third">
+                        <div className="column is-half">
                             <br/>
                             <h1 className="title is-1">All Users</h1>
+                            <AddUser
+                              username={this.state.username}
+                              email={this.state.email}
+                              addUser={this.addUser}
+                              handleChange={this.handleChange}
+                            />
                             <hr/>
-                            <br/>
                             <UsersList users={this.state.users}/>
+
                         </div>
                     </div>
                 </div>
             </section>
-        )
-    }
-};
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+        );
+    };
+}
+ReactDOM.render(<App/>, document.getElementById('root'));
